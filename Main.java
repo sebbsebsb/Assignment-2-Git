cde0e906c6ff0790adb2fe316e56a81c109b7c5c
Sebastian <fourniersebastian2002@gmail.com>
2026-09-03 09:25:47 +0900

    Working to fix program

--- Main.java
+++ Main.java
@@ -1,22 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
+    int I;
+    float R;
+    int D;
+    Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         invokingMethod();
     }
-    public static void invokingMethod() {
-        Scanner scanner = new Scanner(System.in);
+    public void invokingMethod() {
         
         System.out.print("Input required score: ");
         int reqScore = scanner.nextInt();
-        second secondClass = new second();
-        if(secondClass.getter() > reqScore) {
-            System.out.println("QUALIFIED");
-        }
-        else {
-            System.out.println("NOT QUALIFIED");
-        }
+        second secondClass = new compute();
+        // if(secondClass.getter() > reqScore) {
+        //     System.out.println("QUALIFIED");
+        // }
+        // else {
+        //     System.out.println("NOT QUALIFIED");
+        // }
         scanner.close();
     }
+    
+    
+    
+    
+    public float compute() {
+        System.out.print("Enter how many games: ");
+        D = scanner.nextInt();
+        float[] score = new float[D];
+        System.out.print("Enter rate of increase: ");
+        R = scanner.nextFloat();
+        System.out.print("Enter score for first game: ");
+        I = scanner.nextInt();
+        
+        score[0] = I;
+        
+        for (int i = 1; i < D; i++) {
+            score[i]=score[i-1]*R;
+        }
+        return score[D-1];
+    }
+
+
 }
