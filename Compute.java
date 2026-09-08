7c31b4d13ddf7fc6bfcf628533f116ed0d0619c3
Sebastian <fourniersebastian2002@gmail.com>
2026-09-08 09:42:05 +0900

    Some changes to qualified calculator

--- Compute.java
+++ Compute.java
@@ -13,7 +13,12 @@ public class Compute{
         
         System.out.print("Enter how many games: ");
         D = scanner.nextInt();
-        float[] score = new float[D];
+        if (D < 4) {
+            D = 4;
+        } else if (D > 10) {
+            D = 10;
+        }
+        float[] score = new float[D + 1];
         System.out.print("Enter rate of increase: ");
         R = scanner.nextFloat();
         System.out.print("Enter score for first game: ");
@@ -21,12 +26,12 @@ public class Compute{
         
         score[0] = I;
         
-        for (int i = 1; i < D; i++) {
+        for (int i = 1; i < D+1; i++) {
             score[i]=score[i-1]*R;
             System.out.println("Iteration " + i + ": " + score[i]);
         }
         
-        if (score[D-1] < reqScore) {
+        if (score[4] < reqScore) {
             System.out.println("NOT QUALIFIED");
         } else {
             System.out.println("QUALIFIED");
--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 
 public class Main {
-    
 
     public static void main(String[] args) {
         
@@ -9,7 +8,4 @@ public class Main {
         program.compute();
         
     }
-    
-
-
 }
