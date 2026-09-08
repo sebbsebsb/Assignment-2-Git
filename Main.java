b9c88e1a0734eb63bab501b306fab2a3a2efcd3c
Sebastian <fourniersebastian2002@gmail.com>
2026-09-08 09:01:48 +0900

    conflict resolution

--- Main.java
+++ Main.java
@@ -1,22 +1,15 @@
-import java.util.Scanner;
 
 public class Main {
+    
 
     public static void main(String[] args) {
-        invokingMethod();
-    }
-    public static void invokingMethod() {
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Input required score: ");
-        int reqScore = scanner.nextInt();
-        second secondClass = new second();
-        if(secondClass.getter() > reqScore) {
-            System.out.println("QUALIFIED");
-        }
-        else {
-            System.out.println("NOT QUALIFIED");
-        }
-        scanner.close();
+        Compute program = new Compute();
+        
+        program.compute();
+        
     }
+    
+
+
 }
