029bb24dad70a0d80a3fbebf36744c7f353358b7
milk-lactate <r4dylai@gmail.com>
2026-09-10 08:38:27 +0900

    Added readCSV method in Compute.java Aims to read the CSV and output an ArrayList of String array containing its elements. Modified the main compute() function, adding two ArrayLists, with one of them containing the team data and the other their total score and pass status. ReadCSV(), calcScorePass(), and the loop in compute() require testing.

--- Compute.java
+++ Compute.java
@@ -7,8 +7,9 @@ public class Compute{
     int D;
 
     public void compute() {
-        ArrayList<String[]> loadedTeams = readCSV(); // Reads the CSV into this variable
-        ArrayList<Integer> scoreStatus = new ArrayList<Integer>; // Each team will have a scoreStatus at the same idx
+        ArrayList<String[]> loadedTeams = new ArrayList<String[]>(); // Reads the CSV into this variable
+        loadedTeams = readCSV();
+        ArrayList<Integer> scoreStatus = new ArrayList<Integer>(); // Each team will have a scoreStatus at the same idx
 
         Scanner scan = new Scanner(System.in);
         
